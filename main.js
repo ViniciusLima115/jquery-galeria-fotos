dd9ecc0ba321b5cb0da208d5e251f2ba32219abc
Vinicius Lima <viniciusTTM@outlook.com>
2023-03-14 23:29:03 -0700

    fixing bugs

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
-$(document).ready(function(){
-    $('header button').click(function(){
+$(document).ready(function() {
+    $('header button').click(function() {
         $('form').slideDown();
     })
 
-    $('#botao-cancelar').click(function(){
+    $('#botao-cancelar').click(function(e) {
         $('form').slideUp();
     })
 
-    $('form').on('submit',function(e){
+    $('form').on('submit', function(e) {
         e.preventDefault();
+
         const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
-        const novoItem = $('<li style="display: none;"></li>');
-        $(`<img src = "${enderecoDaNovaImagem}" />`).appendTo(novoItem);
+        const novoItem = $('<li style="display: none;"></li>')
+        $(`<img src="${enderecoDaNovaImagem}"/>`).appendTo(novoItem)
         $(`
-            <div class = "overlay-imagem-link">
-                <a href = "${enderecoDaNovaImagem}" target = "_blank" title = "Ver imagem em tamanho real"
-                    Ver Imagem em Tamanho Real
-                <a/>
-            </div>    
-        `).appendTo(novoItem);
-        $(novoItem).appendTo('ul');
-        $(novoItem).fadeIn(1000);
-        $('#endereco-imagem-nova').val('')
+            <div class="overlay-imagem-link">
+                <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
+                    Ver imagem em tamanho real
+                </a>
+            </div>
+        `).appendTo(novoItem)
+        $(novoItem).appendTo('ul')
+        $(novoItem).fadeIn(1500)
+        $('#endereco-imagem-nova').val(' ')
     })
-})    
+})
